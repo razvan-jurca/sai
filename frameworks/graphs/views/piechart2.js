620c9efc0a874732cc274d6d52e264a0d2f2ac51
Razvan Jurca <razvan.jurca@threepillarglobal.com>
2011-01-06 16:23:03 +0200

    Created the pie chart slice rendering methods.

--- frameworks/graphs/views/piechart2.js
+++ frameworks/graphs/views/piechart2.js
@@ -1,8 +1,15 @@
 /*globals Sai*/
 /**
+  
+  
   @author Razvan Jurca
+  @class
 */
 Sai.PieChart2View = Sai.CanvasView.extend(Sai.ChartLegend, {
+  
+  ///
+  /// Chart attributes
+  ///
   
   /**
     Array of numerical data.
@@ -53,6 +60,10 @@ Sai.PieChart2View = Sai.CanvasView.extend(Sai.ChartLegend, {
     */
     halign: 'center',
     /**
+      Wheter to trigger events for slice clicks.
+    */
+    clicks: NO,
+    /**
       Wheter to display the values/percentages on the pie slices.
     */
     values: {
@@ -87,6 +98,20 @@ Sai.PieChart2View = Sai.CanvasView.extend(Sai.ChartLegend, {
   
   displayProperties: ['chartLayout', 'attrs', 'percentage', 'frame'],
   
+  ///
+  /// Chart methods
+  ///
+  
+  /**
+    The event triggered when a slice was clicked. For the events to be called
+    you must set the clicks attribute of the attrs property to YES.
+    
+    @param {Number} sliceIndex The index of the slice that was clicked.
+  */
+  sliceClicked: function(sliceIndex) {
+    console.log('Slice ' + sliceIndex + ' was clicked.');
+  },
+  
   /**
     Renders the chart.
   */
@@ -94,22 +119,53 @@ Sai.PieChart2View = Sai.CanvasView.extend(Sai.ChartLegend, {
     var frame = this.get('frame'),
         layout = this.get('chartLayout') || {},
         attrs = this.get('attrs') || {},
-        region;
+        percentage = this.get('percentage') || [],
+        colors = attrs.colors || [],
+        sliceAttrs = {
+          strokeWidth: SC.none(attrs.strokeWidth) ? 1 : attrs.strokeWidth,
+          stroke: attrs.stroke || 'black'
+        },
+        region, slices, cx, cy, r;
     
     layout.left = layout.left || 0;
     layout.right = layout.right || 0;
     layout.top = layout.top || 0;
     layout.bottom = layout.bottom || 0;
     
+    if (!firstTime) canvas.clear();
     region = this._computeRegion(frame, layout, attrs);
+    r = region.radius;
+    cx = region.left + r;
+    cy = region.top + r;
     
-    this.makeLegend(canvas, frame, layout);
+    slices = this._computeSlices(region, percentage, colors);
+    for (var i=0; i < slices.length; ++ i) {
+      this._renderSlice(canvas, cx, cy, r, slices.objectAt(i), sliceAttrs, attrs.clicks || NO);
+    }
+    if (slices.length === 0) {
+      canvas.circle(cx, cy, r, { stroke: '#777', strokeWidth: 1, fill: '#ddd' }, 'invalid-data-slice');
+    }
+    
+    // TODO [RJ]: draw values/percents
+    
+    this.makeLegend(canvas, frame, region);
   },
   
+  ///
+  /// Chart internals
+  ///
+  
+  /**
+    Computes the region in which to draw the chart.
+    
+    @param {Object} frame The frame of the chart.
+    @param {Object} layout The chart's layout.
+    @param {Object} attrs Chart attributes.
+  */
   _computeRegion: function(frame, layout, attrs) {
     var width = Math.max(frame.width - layout.left - layout.right, 0),
         height = Math.max(frame.height - layout.top - layout.bottom, 0),
-        x = layout.left, y = layout.right,
+        x = layout.left, y = layout.top,
         diameted = Math.min(width, height),
         radius = diameted / 2;
     
@@ -138,5 +194,61 @@ Sai.PieChart2View = Sai.CanvasView.extend(Sai.ChartLegend, {
     }
     
     return { left: ~~x, top: ~~y, right: ~~(x + diameted), bottom: ~~(y + diameted), radius: ~~radius };
+  },
+  
+  /**
+    Computes the slices of the chart.
+    
+    @param {Object} region The region in which to draw the chart.
+    @param {Array} percentage The array of percentages for the input data.
+    @param {Array} colors The colors used to render the slices.
+  */
+  _computeSlices: function(region, percentage, colors) {
+    var result = [], sa, ea = 0,
+        percent = 0,
+        plen = percentage.length;
+    
+    for (var i=0; i < plen; ++ i) {
+      percent += percentage.objectAt(i);
+      if (percentage.objectAt(i) > 0.001) {
+        sa = ea;
+        ea = percent * 360;
+        result.push({
+          index: i,
+          color: colors.objectAt(i) || '#aaa',
+          sangle: sa,
+          eangle: ea
+        });
+      }
+    }
+    
+    return result;
+  },
+  
+  /**
+    Renders a slice of the chart.
+    
+    @param {Sai.Canvas} canvas The canvas onto which to draw.
+    @param {Number} cx The x coordinate of the center of the chart.
+    @param {Number} cy The y coordinate of the center of the chart.
+    @param {Number} r The radius of the chart.
+    @param {Object} slice The slice's properties.
+    @param {Boolean} clicks Wheter to trigger click events.
+    @private
+  */
+  _renderSlice: function(canvas, cx, cy, r, slice, attrs, clicks) {
+    var that = this, x1, x2, y1, y2, path, rad = Math.PI / 180;
+    
+    attrs.fill = slice.color;
+    if (clicks) attrs.click = function() { that.sliceClicked(slice.index); };
+    else attrs.click = null;
+    
+    x1 = cx + r * Math.cos(-slice.sangle * rad);
+    x2 = cx + r * Math.cos(-slice.eangle * rad);
+    y1 = cy + r * Math.sin(-slice.sangle * rad);
+    y2 = cy + r * Math.sin(-slice.eangle * rad);
+    path = ['M', cx, cy, 'L', x2, y2, 'A', r, r, 0, +(Math.abs(slice.eangle - slice.sangle) > 180), 1, x1, y1, 'Z'];
+    
+    canvas.path(path, attrs, 'slice-%@'.fmt(slice.index));
   }
 });
